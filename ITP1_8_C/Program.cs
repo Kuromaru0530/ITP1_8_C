39ff4fc91201c31f8aca88f83ac0d20c2bab4d9b
Kuromaru0530 <kuromary77@gmail.com>
2023-04-10 16:29:55 +0900

    プロジェクト ファイルを追加します。

---
+++ ITP1_8_C/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ITP1_8_C
+{
+    class Program
+    {
+        static void Main()
+        {
+                var set = Enumerable.Range('a', 26).Select(i => (char)i).ToArray();
+                int[] ans = new int[set.Length];
+                List<string> list = new List<string>();
+
+            while(true)
+            {
+                string Input = Console.ReadLine();
+
+                if (Input == null) break;
+
+                else
+                {
+                    list.Add(Input.ToLower());
+                }
+            }
+            string[] lists = list.ToArray();
+            for (int i = 0; i < lists.Length; i++)
+            {
+                foreach (char c in lists[i])
+                    for (int j = 0; j < set.Length; j++)
+                    {
+                        if (set[j] == c)
+                        {
+                            ans[j]++;
+                        }
+                    }
+            }
+           
+            
+            for (int i = 0; i < set.Length; i++)
+            {
+                Console.WriteLine($"{set[i]} : {ans[i]}");
+            }
+        }
+    }
+}
